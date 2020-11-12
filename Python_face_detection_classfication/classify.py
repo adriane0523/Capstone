09287f4a391e908058499129b74b4ce8ce8b87a9
Adriane Inocencio <adriane.0523@gmail.com>
2020-11-12 16:01:34 -0700

    added python face classfications - be able to classify Adriane

--- Python_face_detection_classfication/classify.py
+++ Python_face_detection_classfication/classify.py
@@ -6,42 +6,65 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
 from sklearn.svm import SVC
 from matplotlib import pyplot
-# load faces
-data = load('5-celebrity-faces-dataset.npz')
-testX_faces = data['arr_2']
-# load face embeddings
-data = load('5-celebrity-faces-embeddings.npz')
-trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-# normalize input vectors
-in_encoder = Normalizer(norm='l2')
-trainX = in_encoder.transform(trainX)
-testX = in_encoder.transform(testX)
-# label encode targets
-out_encoder = LabelEncoder()
-out_encoder.fit(trainy)
-trainy = out_encoder.transform(trainy)
-testy = out_encoder.transform(testy)
-# fit model
-model = SVC(kernel='linear', probability=True)
-model.fit(trainX, trainy)
-# test model on a random example from the test dataset
-selection = choice([i for i in range(testX.shape[0])])
-random_face_pixels = testX_faces[selection]
-random_face_emb = testX[selection]
-random_face_class = testy[selection]
-random_face_name = out_encoder.inverse_transform([random_face_class])
-# prediction for the face
-samples = expand_dims(random_face_emb, axis=0)
-yhat_class = model.predict(samples)
-yhat_prob = model.predict_proba(samples)
-# get name
-class_index = yhat_class[0]
-class_probability = yhat_prob[0,class_index] * 100
-predict_names = out_encoder.inverse_transform(yhat_class)
-print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
-print('Expected: %s' % random_face_name[0])
-# plot for fun
-pyplot.imshow(random_face_pixels)
-title = '%s (%.3f)' % (predict_names[0], class_probability)
-pyplot.title(title)
-pyplot.show()
+
+from train import *
+
+
+def classify(file_name):
+
+    # load training date and their face embeddings
+    data = load('5-celebrity-faces-embeddings.npz')
+    trainX, trainy = data['arr_0'], data['arr_1']
+
+    #load image you want to classify - it should be in a folder
+    x = extract_face(file_name)
+
+    #model to extract face
+    model = load_model('facenet_keras.h5')
+    X = list()
+    X.append(x)
+    X = asarray(X)
+    xEmbed = list()
+
+    for face_pixels in X:
+        embedding = get_embedding(model, face_pixels)
+        xEmbed.append(embedding)
+    testX = asarray(xEmbed)
+
+    # normalize input vectors
+    in_encoder = Normalizer(norm='l2')
+    trainX = in_encoder.transform(trainX)
+    testX = in_encoder.transform(testX)
+
+    # label encode targets
+    out_encoder = LabelEncoder()
+    out_encoder.fit(trainy)
+    trainy = out_encoder.transform(trainy)
+
+
+    # fit model
+    model = SVC(kernel='linear', probability=True)
+    model.fit(trainX, trainy)
+
+    # test model on a random example from the test dataset
+    random_face_emb = testX[0]
+
+    # prediction for the face
+    samples = expand_dims(random_face_emb, axis=0)
+    yhat_class = model.predict(samples)
+    yhat_prob = model.predict_proba(samples)
+
+    # get name
+    class_index = yhat_class[0]
+    class_probability = yhat_prob[0,class_index] * 100
+    predict_names = out_encoder.inverse_transform(yhat_class)
+    print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+
+    # plotting it - if you want to view the image
+    pyplot.imshow(x)
+    title = '%s (%.3f)' % (predict_names[0], class_probability)
+    pyplot.title(title)
+    pyplot.show()
+
+if __name__ == "__main__":
+    classify('classify/5.jpg')
--- Python_face_detection_classfication/readme.md
+++ Python_face_detection_classfication/readme.md
@@ -1,3 +1,22 @@
 dependencies:
-
 pip install tensorflow keras matplotlib numpy pillow
+__________________________________________________________________
+
+Inspired by: https://machinelearningmastery.com/how-to-develop-a-face-recognition-system-using-facenet-in-keras-and-an-svm-classifier/
+
+This program will classify any image stored into the classify folder. In the classify.py make sure to reference which image you are referencing. 
+
+Classify Folder – Hold photos you want to classify
+Classify.py – classify image in the classify folder
+Train.py – train model to recognize faces from the train folder
+Train Folder – name of each folder is the name of the user which will hold photos of that user
+.npz file – stores the face arrays and embeddings of the training images
+
+Instructions when trying to classify new faces:
+1.	Create new folder in train with your name and photos of that person
+2.	Run train.py – if the program fails to run due to out of index error from 	x1, y1, width, height = results[0]['box'], that means it can’t recognize a face in one of the folders. If that is the case then crop the photos to be closer to the face or replace with a clearer photo
+3.	After successful run of the program - 5-celebrity-faces-dataset.npz and 5-celebrity-faces-embeddings.npz is written into.
+4.	Input image you want to classify in the classify folder
+5.	Go to classify.py and make sure the directory is pointing to the image you want to classify.
+6.	Run classify.py
+
